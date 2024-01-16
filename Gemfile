728d8e1fd56307de7667c8999d9b97da43e71ca7
Chris <chris@antrapol.com>
2024-01-16 11:51:26 +0800

    Add transfer user support on Linux host

--- Gemfile
+++ Gemfile
@@ -8,3 +8,5 @@ gemspec
 gem "rake", "~> 13.0"
 
 gem "rspec", "~> 3.0"
+
+gem "docker-cli", git: "docker-cli", branch: "master"
--- lib/dockdev/image.rb
+++ lib/dockdev/image.rb
@@ -1,6 +1,10 @@
 
 require 'docker/cli'
 
+require 'securerandom'
+
+require_relative 'user_info'
+
 module Dockdev
   class Image
     include TR::CondUtils
@@ -20,22 +24,39 @@ module Dockdev
     end
 
     def new_container(cont_name, opts = {})
+
       optss = {
         interactive: true,
         tty: true,
-        container_name: cont_name
+        container_name: cont_name,
+        match_user: TR::RTUtils.on_linux?
       }
       optss.merge!(opts)
+
       @cmd_fact.create_container_from_image(@image_name, optss).run
+
     end
 
     def build(dockerfile, opts = {})
+
+      dockerfilePath = dockerfile
+      if is_transfer_user?(opts)
+        cont = append_transfer_user_in_dockerfile(dockerfile)
+        dockerfilePath = generated_dockerfile
+        File.open(dockerfilePath, "w") do |f|
+          f.write cont
+        end
+      end
+
       optss = {  
         context_root: opts[:root],
-        dockerfile: dockerfile
+        dockerfile: dockerfilePath
       }
       optss.merge!(opts)
       @cmd_fact.build_image(@image_name, optss).run
+
+      FileUtils.rm(generated_dockerfile) if File.exist?(generated_dockerfile) and not is_keep_generated_dockerfile?
+
     end
 
     def destroy
@@ -45,6 +66,68 @@ module Dockdev
       else
         raise Error, "Error triggered during deleting image : #{res.err_stream}"
       end
+    end
+
+
+    private
+    def logger
+      Dockdev.logger(:dockdev_image)
+    end
+
+    def is_keep_generated_dockerfile?
+      v =  ENV["DOCKDEV_KEEP_GENERATED_DOCKERFILE"]
+      is_empty?(v) ? false : (v.downcase == "true") ? true : false
+    end
+
+    def generated_dockerfile
+      "Dockerfile-dockdev"
+    end
+
+    def is_transfer_user?(opts = {})
+      if TR::RTUtils.on_linux?
+        true
+      else
+        (opts[:match_user].nil? || not_bool?(opts[:match_user])) ? false : opts[:match_user]
+      end
+    end
+
+    def append_transfer_user_in_dockerfile(file)
+      if File.exist?(file)
+        logger.debug "Append transfer user in dockerfile '#{file}'"
+        res = []
+        cont = File.read(file)
+        indx = cont =~ /CMD/
+        if indx != nil 
+
+          res << cont[0...indx] 
+          res << transfer_user_command
+          res << cont[indx..-1] 
+
+        else
+
+          res << cont
+          res << transfer_user_command
+
+        end
+
+        res.join
+
+      else
+        ""
+      end
+    end
+
+    def transfer_user_command
+
+      ui = UserInfo.user_info
+      gi = UserInfo.group_info
+
+      res = []
+      res << "RUN apt-get update && apt-get install -y sudo"
+      res << "RUN groupadd -f -g #{gi[:gid]} #{gi[:group_name]} && useradd -u #{ui[:uid]} -g #{gi[:gid]} -m #{ui[:login]} && usermod -aG sudo #{ui[:login]} && echo '#{ui[:login]} ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers"
+      res << "USER #{ui[:login]}"
+      res.join("\n")
+
     end
 
   end
